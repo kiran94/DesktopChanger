33499f77bb479f2f4eb5e9ee84951abbe7766dc0
Kiran94 <kiran_patel94@hotmail.com>
2017-04-10 22:17:15 +0100

    added http header to request

--- services/httpService.py
+++ services/httpService.py
@@ -5,4 +5,6 @@ class httpService:
 
     # GET the document from the URL. 
     def get(self, url):
-        return urllib2.urlopen(url).read()
+        headers = { 'User-Agent' : 'Mozilla/5.0' }
+        req = urllib2.Request(url, None, headers)
+        return urllib2.urlopen(req).read()
